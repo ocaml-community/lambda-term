afbc46c5157c13b5d5acb80382018e837e6157e6
Jeremie Dimino <jeremie@dimino.org>
2011-02-28 10:49:01 +0100

    add the colors_256 example
    Ignore-this: 309b328f214e9c2626034e69ab23b74b
    
    darcs-hash:20110228094901-c41ad-5dc5819a48b21ba1dde3d5229d5c30edeb77af57

---
+++ examples/colors_256.ml
@@ -0,0 +1,16 @@
+(*
+ * colors_256.ml
+ * -------------
+ * Copyright : (c) 2011, Jeremie Dimino <jeremie@dimino.org>
+ * Licence   : BSD3
+ *
+ * This file is a part of Lambda-Term.
+ *)
+
+open Lwt
+open Lt_style
+
+lwt () =
+  for_lwt i = 0 to 255 do
+    Lt_term.printls [format "color %d: " i; Foreground(Index i); String "example"; Reset]
+  done
