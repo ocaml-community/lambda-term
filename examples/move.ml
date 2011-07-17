ffd9dfb69ac3e8b9cef37e123a1168dce5a12488
Jeremie Dimino <jeremie@dimino.org>
2011-07-17 20:23:17 +0200

    add Lt_ui
    Ignore-this: 37abff3e406dcb04d1bf4c1e4fc15f7e
    
    darcs-hash:20110717182317-c41ad-05b4eff7aa5ca384a67a6ef7e63d0450ab953bc4

--- examples/move.ml
+++ examples/move.ml
@@ -7,41 +7,52 @@
  * This file is a part of Lambda-Term.
  *)
 
-open CamomileLibraryDyn.Camomile
 open Lwt
 open Lwt_react
 open Lt_geom
 open Lt_text
 open Lt_key
 
+let rec loop ui coord =
+  Lt_ui.loop ui >>= function
+    | Lt_event.Key{ code = Up } ->
+        coord := { !coord with line = !coord.line - 1 };
+        Lt_ui.draw ui;
+        loop ui coord
+    | Lt_event.Key{ code = Down } ->
+        coord := { !coord with line = !coord.line + 1 };
+        Lt_ui.draw ui;
+        loop ui coord
+    | Lt_event.Key{ code = Left } ->
+        coord := { !coord with column = !coord.column - 1 };
+        Lt_ui.draw ui;
+        loop ui coord
+    | Lt_event.Key{ code = Right } ->
+        coord := { !coord with column = !coord.column + 1 };
+        Lt_ui.draw ui;
+        loop ui coord
+    | Lt_event.Key{ code = Escape } ->
+        return ()
+    | ev ->
+        loop ui coord
+
+let draw matrix size coord =
+  let ctx = Lt_draw.context matrix size in
+  Lt_draw.clear ctx;
+  Lt_draw.draw_frame ctx { r_line = 0; r_column = 0; r_lines = size.lines; r_columns = size.columns } Lt_draw.Light;
+  if size.lines > 2 && size.columns > 2 then begin
+    let ctx = Lt_draw.sub ctx { r_line = 1; r_column = 1; r_lines = size.lines - 2; r_columns = size.columns - 2 } in
+    Lt_draw.draw_styled ctx coord.line coord.column (eval [B_fg Lt_style.lblue; S"Move me"; E_fg])
+  end
+
 lwt () =
-  let waiter, wakener = wait () in
-  let coord, set_coord = S.create { line = 0; column = 0 } in
-
-  let draw ctx focused =
-    let coord = S.value coord in
-    Lt_draw.draw_styled ctx coord.line coord.column (eval [B_fg Lt_style.lblue; S"Move me"; E_fg]);
-    None
-  in
-
-  let on_event ev =
-    let coord = S.value coord in
-    match ev with
-      | Lt_event.Key{ code = Up } ->
-          set_coord { coord with line = coord.line - 1 }
-      | Lt_event.Key{ code = Down } ->
-          set_coord { coord with line = coord.line + 1 }
-      | Lt_event.Key{ code = Left } ->
-          set_coord { coord with column = coord.column - 1 }
-      | Lt_event.Key{ code = Right } ->
-          set_coord { coord with column = coord.column + 1 }
-      | Lt_event.Key{ code = Escape } ->
-          wakeup wakener ()
-      | _ ->
-          ()
-  in
-
-  let widget = Lt_widget.make ~can_focus:true ~need_redraw:(E.stamp (S.changes coord) ()) ~draw ~on_event () in
-
   lwt term = Lazy.force Lt_term.stdout in
-  Lt_widget.run term (Lt_widget.frame widget) waiter
+
+  (* Coordinates of the message. *)
+  let coord = ref { line = 0; column = 0 } in
+
+  lwt ui = Lt_ui.create term (fun matrix size -> draw matrix size !coord) in
+  try_lwt
+    loop ui coord
+  finally
+    Lt_ui.quit ui
