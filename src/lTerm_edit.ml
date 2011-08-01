2b19f9a7240c150b926aa2f9172d3e6c6331489d
Jeremie Dimino <jeremie@dimino.org>
2011-08-01 15:18:03 +0200

    avoid a conflict in keybindings
    Ignore-this: 8750d7a60b71730d84a31d1b0521d3de
    
    darcs-hash:20110801131803-c41ad-f77a8c1f2853c3d4d7471e2a531ec4d891e18f81

--- src/lTerm_edit.ml
+++ src/lTerm_edit.ml
@@ -49,7 +49,8 @@ let () =
   { control = false; meta = true; shift = false; code = Left } --> Prev_word;
   { control = true; meta = false; shift = false; code = Right } --> Next_word;
   { control = true; meta = false; shift = false; code = Left } --> Prev_word;
-  { control = true; meta = false; shift = false; code = Char(UChar.of_char 'w') } --> Delete_prev_word;
+  { control = false; meta = true; shift = false; code = Delete } --> Delete_prev_word;
+  { control = true; meta = false; shift = false; code = Delete } --> Delete_next_word;
   { control = false; meta = true; shift = false; code = Char(UChar.of_char 'd') } --> Delete_next_word
 
 (* +-----------------------------------------------------------------+
