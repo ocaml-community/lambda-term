29ff0f34bedfafacdd9d9b8f40a84550fe460464
Jérémie Dimino <jeremie@dimino.org>
2012-10-17 08:36:08 +0200

    Add a Makefile
    It compiles setup.ml to a native executable.

--- setup.ml
+++ setup.ml
@@ -4,14 +4,13 @@
  * Copyright : (c) 2011, Jeremie Dimino <jeremie@dimino.org>
  * Licence   : BSD3
  *
- * This file is a part of Lambda-Term.
+ * This file is a part of Zed, an editor engine.
  *)
 
 (* OASIS_START *)
-(* DO NOT EDIT (digest: 7f47a529f70709161149c201ccd90f0b) *)
 #use "topfind";;
 #require "oasis.dynrun";;
 open OASISDynRun;;
 (* OASIS_STOP *)
 
-let () = setup ()
+let () = setup ();;
