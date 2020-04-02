ec5478d26c8e3106e81319b834acf7dcb948647d
ZAN DoYe <zandoye@gmail.com>
2020-04-03 00:06:32 +0800

    LTerm_vi: add Query module, including some utils

--- src/lTerm_vi.ml
+++ src/lTerm_vi.ml
@@ -20,6 +20,175 @@ module Concurrent = struct
     end
 end
 
+module Query = struct
+  (*
+  (* left right *)
+  | Left of int (* h *)
+  | Right of int (* l *)
+  | Line_FirstChar of int (* 0 *)
+  | Line_FirstNonBlank of int (* ^ *)
+  | Line_LastChar of int (* $ *)
+  | Line_LastNonBlank of int (* g_ *)
+
+  (* up down *)
+  | Upward of int (* k *)
+  | Downward of int (* j *)
+  | GotoLine of int (* gg or G *)
+  | GotoLine_first (* gg *)
+  | GotoLine_last (* G *)
+
+  (* word *)
+  | Word of int (* w *)
+  | WORD of int (* W *)
+  | Word_end of int (* e *)
+  | WORD_end of int (* E *)
+  | Word_back of int (* b *)
+  | WORD_back of int (* B *)
+  | Word_back_end of int (* ge *)
+  | WORD_back_end of int (* gE *)
+
+  (* text object *)
+  | Sentence_backword of int (* ( *)
+  | Sentence_forward of int (* ) *)
+  | Paragraph_backward of int (* { *)
+  | Paragraph_forward of int (* } *)
+
+  (* text object selection *)
+  | Word_include of int (* aw *)
+  | Word_inner of int (* iw *)
+  | WORD_include of int (* aW *)
+  | WORD_inner of int (* iW *)
+  | Sentence_include of int (* as *)
+  | Sentence_inner of int (* is *)
+  | Paragraph_include of int (* ap *)
+  | Paragraph_inner of int (* ip *)
+  *)
+  let left n ctx= (* h *)
+    let edit= Zed_edit.edit ctx in
+    let lines= Zed_edit.lines edit
+    and line_idx= Zed_edit.line ctx in
+    let line_len= Zed_lines.line_length lines line_idx in
+    let column= Zed_edit.column ctx in
+    let dest= (column - n) |> max 0 |> min line_len in
+    if dest > column then
+      (column, dest - column)
+    else
+      (dest, column - dest)
+
+  let right n ctx= (* l *)
+    let edit= Zed_edit.edit ctx in
+    let lines= Zed_edit.lines edit
+    and line_idx= Zed_edit.line ctx in
+    let line_len= Zed_lines.line_length lines line_idx in
+    let column= Zed_edit.column ctx in
+    let dest= (column + n) |> max 0 |> min line_len in
+    if dest > column then
+      (column, dest - column)
+    else
+      (dest, column - dest)
+
+  let line_FirstChar _n ctx= (* 0 *)
+    let edit= Zed_edit.edit ctx in
+    let lines= Zed_edit.lines edit
+    and line_idx= Zed_edit.line ctx in
+    let start= Zed_lines.line_start lines line_idx in
+    let column= Zed_edit.column ctx in
+    (start, column - start)
+
+  let line_FirstNonBlank _n ctx= (* ^ *)
+    let edit= Zed_edit.edit ctx in
+    let lines= Zed_edit.lines edit
+    and line_idx= Zed_edit.line ctx in
+    let start= Zed_lines.line_start lines line_idx in
+    let line_len= Zed_lines.line_length lines line_idx in
+    start, line_len
+
+  open CamomileLibraryDefault.Camomile
+
+  let get_category ?(nl_as_sp=false) uchar=
+    if uchar = Zed_utf8.extract "\n" 0 && nl_as_sp then
+      `Zs
+    else
+      UCharInfo.general_category uchar
+
+  let get_boundary multi_line ctx=
+    let edit= Zed_edit.edit ctx in
+    if multi_line then
+      (0, Zed_rope.length (Zed_edit.text edit))
+    else
+      let lines= Zed_edit.lines edit
+      and line_idx= Zed_edit.line ctx in
+      (Zed_lines.line_start lines line_idx
+      , Zed_lines.line_stop lines line_idx)
+
+  let next_category ?(nl_as_sp=true) ~pos ~stop text=
+    let start_category=
+      let zchar= Zed_rope.get text pos in
+      let core= Zed_char.core zchar in
+      get_category ~nl_as_sp core
+    in
+    let zip= Zed_rope.Zip.make_f text pos in
+    let rec skip_curr zip pos=
+      if pos < stop then
+        let zchar, zip= Zed_rope.Zip.next zip in
+        let category= get_category ~nl_as_sp (Zed_char.core zchar) in
+        if category = start_category then
+          skip_curr zip (pos + 1)
+        else
+          pos
+      else
+        pos
+    in
+    skip_curr zip pos
+
+  let prev_category ?(nl_as_sp=true) ~pos ~start text=
+    let start_category=
+      let zchar= Zed_rope.get text pos in
+      let core= Zed_char.core zchar in
+      get_category ~nl_as_sp core
+    in
+    let zip= Zed_rope.Zip.make_f text pos in
+    let rec skip_curr zip pos=
+      if pos > start then
+        let zchar, zip= Zed_rope.Zip.prev zip in
+        let category= get_category ~nl_as_sp (Zed_char.core zchar) in
+        if category = start_category then
+          skip_curr zip (pos - 1)
+        else
+          pos
+      else
+        pos
+    in
+    (skip_curr zip pos) - 1
+
+  let next_word ?(multi_line=true) ctx=
+    let nl_as_sp= multi_line in
+    let edit= Zed_edit.edit ctx in
+    let text= Zed_edit.text edit in
+    let pos= Zed_edit.position ctx in
+    let _start, stop= get_boundary multi_line ctx in
+    let start_category=
+      let zchar= Zed_rope.get text pos in
+      let core= Zed_char.core zchar in
+      get_category ~nl_as_sp core
+    in
+    let next= next_category ~nl_as_sp ~pos ~stop text in
+    if start_category = `Zs then
+      next (* currently at a space, just skip spaces *)
+    else
+    if next < stop then
+      (* skip potential subsequent spaces after skip current word*)
+      let zchar= Zed_rope.get text pos in
+      let core= Zed_char.core zchar in
+      if get_category ~nl_as_sp core = `Zs then
+        (* skip subsequent spaces *)
+        next_category ~nl_as_sp ~pos ~stop text
+      else
+        next
+    else
+      next
+end
+
 module Vi = Mew_vi.Core.Make (Concurrent)
 include Vi
 
